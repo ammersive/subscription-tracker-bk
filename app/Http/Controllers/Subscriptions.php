3a4a67410130cf0d13f307523101d40fb2db299d
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-14 15:48:30 +0000

    Created Subscription model and drafted controller

---
+++ app/Http/Controllers/Subscriptions.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Subscription;
+
+class Subscriptions extends Controller
+{
+    public function index()
+    {
+        return view("subscriptions", [
+            "subscriptions" => Subscription::all(),   
+        ]);
+    }
+
+    public function show($id)
+    {
+        $subscription = Subscription::find($id);
+
+        return view("subscription", [
+            "subscription" => $subscription
+        ]);
+    }
+}
