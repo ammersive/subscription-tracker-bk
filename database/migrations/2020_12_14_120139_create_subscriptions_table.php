43ae72f5405a12708f85173d6e6a645113b5c9ae
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-14 13:05:46 +0000

    Created subscriptions table and migrated

---
+++ database/migrations/2020_12_14_120139_create_subscriptions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string("subscription_name");
+            $table->decimal("cost", $precision = 8, $scale = 2);
+            $table->bigInteger("start");
+            $table->integer("payment_date");
+            $table->integer("notice_period");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
