852a17eb4b408b0260b3f45e482b8c76da5fd279
Tumeka Burston <burston1993@icloud.com>
2020-12-15 12:26:01 +0000

    Created categories table with subscription_category pivot table

---
+++ database/migrations/2020_12_15_121118_create_categories_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("categories", function (Blueprint $table) {
+            $table->id();
+            $table->string("category_name", 50);
+        });
+
+        //pivot table
+        Schema::create("subscription_category", function (Blueprint $table) {
+            // still have an id column
+            $table->id();
+            // create the article id column and foreign key
+            $table->foreignId("subscription_id")
+                ->constrained()
+                ->onDelete("cascade");
+            // create the tag id column and foreign key
+            $table->foreignId("category_id")
+                ->constrained()
+                ->onDelete("cascade");
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::dropIfExists('subscription_category');
+        Schema::dropIfExists('categories');
+    }
+}
